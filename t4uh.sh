0920dc63e4c12ee1a257ab334f978549bedf7ef1
sml156 <sml156@hotmail.com>
2019-12-09 17:42:23 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -9,7 +9,7 @@ apt -y autoremove
 # airgeddon, Even though it's not working properly right now
 git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git 1>/dev/null && echo "OK airgeddon Downloaded"  || echo "airgeddon Failed"
 bash /airgeddon/airgeddon.sh
-ls
+
 
 # 2 or 3 of these is for airgeddon, about 3 are for wifite but
 # the rest are for stuff that I probably will never remember 
@@ -23,18 +23,20 @@ apt -y install $(cat /add-packages/add-packages-list.txt | xargs)
 #apt -y install linux-headers-`uname -r`
 
 # Install the t4uh driver
-#git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git 1>/dev/null && echo "OK t4uh driver"  || echo "t4uh driver"
-#bash /rtl8812au/dkms-install.sh
-#ls
+git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git 1>/dev/null && echo "OK t4uh driver"  || echo "t4uh driver"
+make
+make install
+bash /rtl8812au/dkms-install.sh
+
 # Wifite is broken, It needs to be reinstalled
 git clone https://github.com/derv82/wifite2.git 1>/dev/null && echo "OK Wifite Downloaded"  || echo "Wifite Failed"
 python /wifite2/setup.py install
-ls
 
-git clone https://github.com/ZerBea/hcxdumptool.git
-cd hcxdumptool
-make
-make install
+
+#git clone https://github.com/ZerBea/hcxdumptool.git
+#cd hcxdumptool
+#make
+#make install
 
 echo " ##########################################################  "
 echo " You are going To Reboot in 10 sec's  "
