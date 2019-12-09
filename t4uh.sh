0fcf65675204036ce05645eac10c0355f534cbd5
sml156 <sml156@hotmail.com>
2019-12-09 09:14:15 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -1,25 +1,24 @@
-#wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/update.txt > update.txt
-#git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git
+wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/update.txt > update.txt
+git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git
 
 
-# apt update && apt -y upgrade
-# "clean up some crap"
+"clean up some crap"
 apt -y autoclean
 apt -y autoremove
 
 
-# Download extra stuff
-#apt -y install $(cat update.txt | xargs)
+echo " Download extra stuff  "
+apt -y install $(cat update.txt | xargs)
 
-#apt -y install linux-headers-`uname -r`
+apt -y install linux-headers-`uname -r`
 
 
 
-#cd rtl*
-#bash dkms-install.sh
+cd rtl*
+bash dkms-install.sh
 
-##########################################################
+echo " ##########################################################  "
 echo " You Need To Reboot now  "
-##########################################################
+echo " ##########################################################  "
 
 
