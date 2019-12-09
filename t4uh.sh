ac103783bc02971a7efe0cf810ae18f5235ab9b6
sml156 <sml156@hotmail.com>
2019-12-09 13:49:45 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -1,4 +1,4 @@
-#apt update && apt -y upgrade
+apt update && apt -y upgrade
 
 mkdir t4uh-setup
 cd t4uh-setup
@@ -11,12 +11,12 @@ apt -y autoremove
 git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git 1>/dev/null && echo "OK airgeddon Downloaded"  || echo "airgeddon Failed"
 cd airgeddon
 ls
-#bash airgeddon.sh
+bash airgeddon.sh
 
 # 2 or 3 of these is for airgeddon, about 3 are for wifite but
 # the rest are for stuff that I probably will never remember 
 wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/add-packages-list.txt > add-packages-list.txt 1>/dev/null && echo "OK add-packages Downloaded"  || echo "add-packages Failed"
-#apt -y install $(cat add-packages-list.txt | xargs)
+apt -y install $(cat add-packages-list.txt | xargs)
 
 # This is only needed if the kernel gets updated, It's harmless if it has not been.
 # If your USB adapter stops working one day your going to have to restore a backup
@@ -28,13 +28,13 @@ apt -y install linux-headers-`uname -r`
 git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git 1>/dev/null && echo "OK t4uh driver"  || echo "t4uh driver"
 cd rtl8812au
 ls
-#bash dkms-install.sh
+bash dkms-install.sh
 
 # Wifite is broken, It needs to be reinstalled
 git clone https://github.com/derv82/wifite2.git 1>/dev/null && echo "OK Wifite Downloaded"  || echo "Wifite Failed"
 cd wifite2
 ls
-#python setup.py install
+python setup.py install
 
 echo " ##########################################################  "
 echo " You Need To Reboot now  "
