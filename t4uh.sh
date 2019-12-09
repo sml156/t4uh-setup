b3f8fa2818c7a616460b6d8a9b752261f6388da6
sml156 <sml156@hotmail.com>
2019-12-09 08:46:02 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -1,19 +1,21 @@
 wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/update.txt > update.txt
 sleep 5
 
-apt update && apt upgrade
+apt update && apt -y upgrade
 echo "clean up some crap"
 apt -y autoclean
 apt -y autoremove
 
-echo "install stuff"
+
 
 apt install $(cat update.txt | xargs)
 
 echo"This will probably error but it don't matter"
 apt install linux-headers-`uname -r`
 
-echo "Install the USB drivers"
+
 git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git
 cd rtl*
 bash dkms-install.sh
+
+
