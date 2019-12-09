454143801de4bcab5c10772678072f37a9bc6ce6
sml156 <sml156@hotmail.com>
2019-12-09 08:48:20 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -1,14 +1,14 @@
 wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/update.txt > update.txt
 sleep 5
 
-apt update && apt -y upgrade
+apt update && apt upgrade -y
 echo "clean up some crap"
 apt -y autoclean
 apt -y autoremove
 
 
 
-apt install $(cat update.txt | xargs)
+apt install -y $(cat update.txt | xargs)
 
 echo"This will probably error but it don't matter"
 apt install linux-headers-`uname -r`
