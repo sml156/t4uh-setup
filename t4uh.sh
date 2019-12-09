1ecd866618adcfd257b67169b6b12dea034e0f86
sml156 <sml156@hotmail.com>
2019-12-09 11:13:33 -0500

    Update t4uh.sh

--- t4uh.sh
+++ t4uh.sh
@@ -1,3 +1,4 @@
+apt update && apt -y upgrade
 wget -qO - https://raw.githubusercontent.com/sml156/t4uh-setup/master/update.txt > update.txt
 git clone -b v5.2.20 https://github.com/aircrack-ng/rtl8812au.git
 
